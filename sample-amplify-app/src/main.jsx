5cd159b61a300edefe548a339b53d8792890df67
Kevon Mayers <novekm@amazon.com>
2023-03-11 02:07:32 -0500

    disable react strictMode for testing

--- sample-amplify-app/src/main.jsx
+++ sample-amplify-app/src/main.jsx
@@ -1,13 +1,15 @@
+/* eslint-disable no-undef */
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './pages/App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  // <React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+  // </React.StrictMode>
 );
