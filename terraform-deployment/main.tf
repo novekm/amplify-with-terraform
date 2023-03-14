2677a304f556eea1b5706abf5584443e5df27d95
Kevon Mayers <novekm@amazon.com>
2023-03-13 20:49:17 -0400

    update main.tf

--- terraform-deployment/main.tf
+++ terraform-deployment/main.tf
@@ -4,33 +4,21 @@ module "sample-qs" {
   // location of the module - can be local or git repo
   source = "./modules/sample-module"
 
-
-  sample_create_codecommit_repo = false
-
-  sample_enable_gitlab_mirroring = false
-
-  sample_existing_repo_url = "https://github.com/novekm/aws-amplify-cloudscape"
-
-  # - SSM -
-  lookup_ssm_github_access_token = true // find the github access token in ssm
-  ssm_github_access_token_name = "github-access-token" // name of your ssm parameter
-
-
   # - Cognito -
   # Admin Users to create
   sample_admin_cognito_users = {
     NarutoUzumaki : {
-      username       = "admin"
-      given_name     = "Default"
-      family_name    = "Admin"
-      email          = "novekm@amazon.com"
+      username       = "nuzumaki"
+      given_name     = "Naruto"
+      family_name    = "Uzumaki"
+      email          = "naruto@rasengan.com"
       email_verified = true // no touchy
     },
     SasukeUchiha : {
-      username       = "kmayers"
-      given_name     = "Kevon"
-      family_name    = "Mayers"
-      email          = "kevonmayers31@gmail.com"
+      username       = "suchiha"
+      given_name     = "Sasuke"
+      family_name    = "Uchiha"
+      email          = "sasuke@chidori.com"
       email_verified = true // no touchy
     }
   }
@@ -40,7 +28,7 @@ module "sample-qs" {
       username       = "default"
       given_name     = "Default"
       family_name    = "User"
-      email          = "kevon_mayers@yahoo.com"
+      email          = "example@example.com"
       email_verified = true // no touchy
     }
   }
